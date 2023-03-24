9c3bb84e5a83a51b80507d06ccb6b6a8301e2c10
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-03-24 03:43:34 +0100

    removed migrate 7 in core

--- core/migrations/0008_alter_product_id.py
+++ core/migrations/0008_alter_product_id.py
@@ -7,7 +7,9 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0007_alter_product_id'),
+        # ('core', '0007_alter_product_id'),
+        ('core', '0006_product_is_approved'),
+        
     ]
 
     operations = [
--- mstore/settings.py
+++ mstore/settings.py
@@ -119,7 +119,8 @@ if _TRY_LOCAL_DB:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3-2',
+        'NAME': BASE_DIR / 'db.sqlite3-3',
+        # 'NAME': BASE_DIR / 'db.sqlite3-2',
         # 'NAME': BASE_DIR / 'db.sqlite3-1',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         }
